1278ca8660fc09fc4d37048c5d4beb4fc1d1d5a4
Johnmorris <135210236+Ochiengmorris@users.noreply.github.com>
2024-02-27 16:17:54 +0000

    added application pages as back to database queries

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, DateTime, text
+from sqlalchemy import create_engine, text
 import os
 
 db_conection_string = os.environ['DB_CONNECTION_STRING']
@@ -16,6 +16,9 @@ def load_jobs_from_db():
 
     return jobs
 
+# new = load_jobs_from_db()
+# print(new)
+
 def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
@@ -26,3 +29,16 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+def add_applications_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, fullName, email, linkedin_url, resume_url, work_experience, education) VALUES (:job_id, :fullName, :email, :linkedin_url, :resume_url, :work_experience, :education)")
+    conn.execute(query, {
+      'job_id': job_id, 
+      'fullName': data['fullName'], 
+      'email': data['email'], 
+      'linkedin_url': data['linkedin_url'], 
+      'resume_url': data['resume_url'], 
+      'work_experience': data['work_experience'], 
+      'education': data['education']
+    })
